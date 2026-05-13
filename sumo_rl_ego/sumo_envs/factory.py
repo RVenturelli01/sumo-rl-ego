5a8c514f72a758123f4ad73aba4cf4eff93cd6a2
RVenturelli01 <riccardo.ventu01@gmail.com>
2026-05-13 14:31:40 +0200

    minor updates

--- sumo_rl_ego/sumo_envs/factory.py
+++ sumo_rl_ego/sumo_envs/factory.py
@@ -3,7 +3,7 @@ from .registry import ENV_REGISTRY
 
 
 
-def make_env(env_id: str, seed: int, **kwargs):
+def make_env(env_id: str, seed: int = 0, **kwargs):
 
     if env_id not in ENV_REGISTRY:
         raise ValueError(f"Unknown env_id '{env_id}'")
@@ -14,7 +14,7 @@ def make_env(env_id: str, seed: int, **kwargs):
 
 
 
-def make_vec_env(env_id: str, n_envs: int, base_seed: int = 0, **kwargs):
+def make_vec_env(env_id: str, n_envs: int, base_seed: int = 0, vec_env_cls = None, **kwargs):
 
     def make_thunk(rank: int):
         def _init():
@@ -23,7 +23,11 @@ def make_vec_env(env_id: str, n_envs: int, base_seed: int = 0, **kwargs):
             return env
         return _init
 
-    vec_env_cls = DummyVecEnv if n_envs == 1 else SubprocVecEnv
+    if n_envs == 1:
+        vec_env_cls = DummyVecEnv
+    elif vec_env_cls == None:
+        vec_env_cls = SubprocVecEnv
+
     env = vec_env_cls([make_thunk(i) for i in range(n_envs)])
 
     return env
